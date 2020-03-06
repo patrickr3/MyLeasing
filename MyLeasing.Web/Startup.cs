ebf795c7b256d2d95b4c59f5c34b0ca6592e8c5b
Patrick Reynoso <patrickr3@gmail.com>
2020-03-06 15:55:02 -0400

    Database ready

--- MyLeasing.Web/Startup.cs
+++ MyLeasing.Web/Startup.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -31,6 +26,7 @@ namespace MyLeasing.Web
                     Configuration.GetConnectionString("DefaultConnection")
                     )
                 );
+            services.AddTransient<SeedDb>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
